54b3c70f531d3c66bbda7b60b1df7eda21af6c4d
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-10-14 14:23:08 -0300

    Adds swagger dependency

---
+++ src/main/java/ravelo/controller/UserController.java
@@ -0,0 +1,36 @@
+package ravelo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ravelo.model.User;
+import ravelo.service.UserService;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        var user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User userToCreate){
+        var userCreated = userService.createUser(userToCreate);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+
+}
